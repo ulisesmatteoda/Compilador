9c02dfb281c7cd4e01db24f5be4d853bfdf123fb
ulisesmatteoda <ulisesmatteoda7@gmail.com>
2025-09-16 19:48:12 -0300

    Agregue algunos tokens

--- Lexico.py
+++ Lexico.py
@@ -6,5 +6,33 @@ class Lexico(Lexer):
 
     tokens = {SUMA, RESTA, MULTIPLICACION, DIVISION, ASIGNACION1,
             ASIGNACION2, LE, GE, LT, GT, NE, IGUAL, ID, IF, ELSE, ENDIF, PRINT, RETURN, 
-            UINT, DFLOAT, DO, WHILE}
+            UINT, DFLOAT, DO, WHILE, FLECHA}
+    
+    #literales
+
+    literals = { '(', ')', '{', '}', ';' }
+
+    #caracteres a ignorar
+
+    ignore = ' \t'
+
+    #reglas para tokens.
+
+    FLECHA = r'->'
+    RESTA = r'\-'
+    SUMA =  r'\+'
+    MULTIPLICACION = r'\*'
+    DIVISION = r'/'
+    IGUAL = r'=='
+    ASIGNACION1 = r'='
+    ASIGNACION2= r':='
+    LE = r'<='
+    LT = r'<'
+    GE = r'>='
+    GT = r'>'
+    NE = r'!='
+    
+
+
+
 
